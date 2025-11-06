5b5f385590d3005fd872970ad65b6d1ff4bf6465
Sakamoto2004 <phbtoan9185@gmail.com>
2025-11-06 19:27:54 +0700

    Added happy case vnpay

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.prm392_finalproject"
-        minSdk = 36
+        minSdk = 34
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
